aec03e9aa75f72bf5137feb24643fcb6a0c2f644
Zorkyx22 <icu.zorkyx@gmail.com>
2023-12-21 23:29:13 -0500

    overhaul to make it work with shuttle

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,65 @@
-use std::env;
-
+use anyhow::anyhow;
 use serenity::async_trait;
-use serenity::prelude::*;
 use serenity::model::channel::Message;
-use serenity::framework::standard::macros::{command, group};
-use serenity::framework::standard::{StandardFramework, Configuration, CommandResult};
+use serenity::model::gateway::Ready;
+use serenity::prelude::*;
+use shuttle_secrets::SecretStore;
+use tracing::{error, info};
+use serenity::model::id::GuildId;
+use serenity::model::prelude::*;
 
-#[group]
-#[commands(ping)]
-struct General;
-
-struct Handler;
+struct Bot {
+    guild: GuildId,
+}
 
 #[async_trait]
-impl EventHandler for Handler {}
+impl EventHandler for Bot {
+    async fn ready(&self, ctx: Context, ready: Ready) {
 
-#[tokio::main]
-async fn main() {
-    let framework = StandardFramework::new().group(&GENERAL_GROUP);
-    framework.configure(Configuration::new().prefix("~")); // set the bot's prefix to "~"
+        info!("{} is connected!", ready.user.name);
+        let commands = self.guild.set_application_commands(&ctx.http, |commands| {
+            commands.create_application_command(|command| { command.name("hello").description("Say hello") })
+        
+        }).await.unwrap();
 
-    // Login with a bot token from the environment
-    let token = env::var("DISCORD_TOKEN").expect("token");
-    let intents = GatewayIntents::MESSAGE_CONTENT;
-    let mut client = Client::builder(token, intents)
-        .event_handler(Handler)
-        .framework(framework)
-        .await
-        .expect("Error creating client");
+        info!("{:#?}", commands);
+    }
 
-    // start listening for events by starting a single shard
-    if let Err(why) = client.start().await {
-        println!("An error occurred while running the client: {:?}", why);
+    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
+
+        if let Interaction::ApplicationCommand(command) = interaction {
+            let response_content = match command.data.name.as_str() {
+                "hello" => "World!".to_owned(),
+                command => unreachable!("Unknown command: {}", command),
+            };
+
+            let create_interaction_response =
+                command.create_interaction_response(&ctx.http, |response| {
+                    response
+                        .kind(InteractionResponseType::ChannelMessageWithSource)
+                        .interaction_response_data(|message| message.content(response_content))
+                });
+
+            if let Err(why) = create_interaction_response.await {
+                eprintln!("Cannot respond to slash command: {}", why);
+            }
+        }
     }
 }
 
-#[command]
-async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
-    msg.reply(ctx, "Pong!").await?;
+#[shuttle_runtime::main]
+async fn serenity(
+    #[shuttle_secrets::Secrets] secret_store: SecretStore,
+) -> shuttle_serenity::ShuttleSerenity {
+    let token = secret_store.get("DISCORD_TOKEN").unwrap();
+    let guild_id = secret_store.get("GUILD_ID").unwrap();
 
-    Ok(())
+    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
+
+    let client = Client::builder(&token, intents)
+        .event_handler(Bot {guild: GuildId(guild_id.parse().unwrap())})
+        .await
+        .expect("Err creating client");
+
+    Ok(client.into())
 }
